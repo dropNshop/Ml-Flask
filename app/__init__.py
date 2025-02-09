baac1b82a4e93b87aa92a07c764692f8e0d0d02c
Osama-tec <hashmiosama555@gmail.com>
2025-02-09 21:04:37 +0500

    updated

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,6 @@
 # This file can be empty, it's just to mark the directory as a Python package 
+
+from app.app import app
+
+# This ensures the app is available when importing the package
+__all__ = ['app'] 
