f0d1033eb1b3cad93992c776c26f52fa37cbf6cd
dropNshop <sajjadgrocery.pharmacy@gmail.com>
2025-02-13 02:03:52 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.metrics import mean_squared_error, r2_score
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta  
 import os
 import calendar
 from scipy import stats
